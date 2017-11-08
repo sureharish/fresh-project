795310017f25873b86dfc45cdb11a6e225939995
sureharish <sureharish63@gmail.com>
2017-11-08 10:17:39 +0530

    Update DemoFreshProjectApplication.java

--- src/main/java/com/example/demoFreshproject/DemoFreshProjectApplication.java
+++ src/main/java/com/example/demoFreshproject/DemoFreshProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoFreshProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("making changes in the github"):
 		SpringApplication.run(DemoFreshProjectApplication.class, args);
 	}
 }
