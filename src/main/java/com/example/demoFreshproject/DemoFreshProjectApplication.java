33f9fd80a9a1c54478ea048e2cc951dca9a258e7
sure harish <hsure@nisum.com>
2017-11-08 10:03:19 +0530

    all files are commited in fresh-project

---
+++ src/main/java/com/example/demoFreshproject/DemoFreshProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoFreshproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFreshProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFreshProjectApplication.class, args);
+	}
+}
